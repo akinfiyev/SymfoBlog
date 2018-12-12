cb5626a62b79314e08b3e7c77116264012e145bd
sliceice <mr.sliceice@gmail.com>
2018-12-13 00:13:04 +0200

    User listing add to admin panel

--- src/Controller/AdminPanelController.php
+++ src/Controller/AdminPanelController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -60,7 +61,95 @@ class AdminPanelController extends AbstractController
 
             return $this->redirect($request->headers->get('referer'));
         } else {
-            throw $this->createNotFoundException('The article does not exist.');;
+            throw $this->createNotFoundException('The article does not exist.');
+        }
+    }
+
+    /**
+     * @Route("/admin/users/", name="users_list")
+     */
+    public function userListingAction (Request $request, ContainerInterface $container)
+    {
+        $users = $this->getDoctrine()
+            ->getRepository(User::class)
+            ->findAll();
+
+        $paginator = $container->get('knp_paginator');
+        $users = $paginator->paginate(
+            $users,
+            $request->query->getInt('page', 1),
+            $request->query->getInt('limit', 5)
+        );
+
+        return $this->render('admin_panel/users_list.html.twig', [
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * @Route("/admin/users/make_blogger/", name="make_user_blogger")
+     */
+    public function makeUserBloggerAction(Request $request)
+    {
+        $user_id = $request->get('user_id');
+        if ($user_id !== null) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $em->getRepository(User::class)
+                ->find($user_id);
+            if (!$user) {
+                throw $this->createNotFoundException('The user does not exist.');
+            }
+
+            if (in_array('ROLE_BLOGGER', $user->getRoles()))
+            {
+                $roles = $user->getRoles();
+                if (($key = array_search('ROLE_BLOGGER', $roles)) !== false)
+                {
+                    unset($roles[$key]);
+                }
+                $user->setRoles($roles);
+            } else {
+                $roles = $user->getRoles();
+                $roles[] = 'ROLE_BLOGGER';
+                $user->setRoles($roles);
+            }
+            $em->merge($user);
+            $em->flush();
+
+            return $this->redirect($request->headers->get('referer'));
+        } else {
+            throw $this->createNotFoundException('The user does not exist.');
+        }
+    }
+
+    /**
+     * @Route("/admin/users/ban/", name="ban_user")
+     */
+    public function banUserAction(Request $request)
+    {
+        $user_id = $request->get('user_id');
+        if ($user_id !== null) {
+            $em = $this->getDoctrine()->getManager();
+
+            $user = $em->getRepository(User::class)
+                ->find($user_id);
+            if (!$user) {
+                throw $this->createNotFoundException('The user does not exist.');
+            }
+
+            if (in_array('ROLE_BANNED', $user->getRoles()))
+            {
+                $user->setRoles(['ROLE_USER']);
+            } else {
+                $user->setRoles(['ROLE_BANNED']);
+            }
+            $em->merge($user);
+            $em->flush();
+
+            return $this->redirect($request->headers->get('referer'));
+        } else {
+            throw $this->createNotFoundException('The user does not exist.');
         }
     }
 }
