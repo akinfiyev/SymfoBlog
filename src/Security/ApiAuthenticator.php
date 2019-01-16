5c9b5032288f415a6ffe0f29402091ec0ed00012
sliceice <mr.sliceice@gmail.com>
2019-01-16 18:17:38 +0200

    ApiAuthenticator create and config

--- src/Security/ApiAuthenticator.php
+++ src/Security/ApiAuthenticator.php
@@ -2,7 +2,11 @@
 
 namespace App\Security;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
 use Symfony\Component\Security\Core\User\UserInterface;
@@ -11,43 +15,89 @@ use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 class ApiAuthenticator extends AbstractGuardAuthenticator
 {
-    public function supports(Request $request)
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
     {
-        // todo
+        $this->em = $em;
     }
 
+    /**
+     * Called on every request to decide if this authenticator should be
+     * used for the request. Returning false will cause this authenticator
+     * to be skipped.
+     */
+    public function supports(Request $request)
+    {
+        return $request->headers->has('X-AUTH-TOKEN');
+    }
+
+    /**
+     * Called on every request. Return whatever credentials you want to
+     * be passed to getUser() as $credentials.
+     */
     public function getCredentials(Request $request)
     {
-        // todo
+        return [
+            'token' => $request->headers->get('X-AUTH-TOKEN'),
+        ];
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        $apiToken = $credentials['token'];
+
+        if (null === $apiToken) {
+            return;
+        }
+
+        // if a User object, checkCredentials() is called
+        return $this->em->getRepository(User::class)
+            ->findOneBy(['apiToken' => $apiToken]);
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
-    }
+        // check credentials - e.g. make sure the password is valid
+        // no credential check is needed in this case
 
-    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
-    {
-        // todo
+        // return true to cause authentication success
+        return true;
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
-        // todo
+        // on success, let the request continue
+        return null;
     }
 
+    public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
+    {
+        $data = [
+            'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
+
+            // or to translate this message
+            // $this->translator->trans($exception->getMessageKey(), $exception->getMessageData())
+        ];
+
+        return new JsonResponse($data, Response::HTTP_FORBIDDEN);
+    }
+
+    /**
+     * Called when authentication is needed, but it's not sent
+     */
     public function start(Request $request, AuthenticationException $authException = null)
     {
-        // todo
+        $data = [
+            // you might translate this message
+            'message' => 'Authentication Required'
+        ];
+
+        return new JsonResponse($data, Response::HTTP_UNAUTHORIZED);
     }
 
     public function supportsRememberMe()
     {
-        // todo
+        return false;
     }
 }
