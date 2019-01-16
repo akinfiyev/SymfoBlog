16685d7b65690fe39b22478279c7279fb543c2d6
sliceice <mr.sliceice@gmail.com>
2019-01-16 13:57:22 +0200

    UserLikes functionality edit

--- src/Controller/UserLikeController.php
+++ src/Controller/UserLikeController.php
@@ -5,56 +5,38 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\UserLike;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class LikeController extends AbstractController
+class UserLikeController extends AbstractController
 {
     /**
-     * @Route("/like", name="like")
+     * @Route("/like/{id}", name="like_article")
      */
-    public function index(Request $request)
+    public function likeArticleAction(Request $request, Article $article)
     {
-        $securityContext = $this->container->get('security.authorization_checker');
-        if ($securityContext->isGranted('IS_AUTHENTICATED_FULLY')) {
-            $referer = $this->redirect($request
+        if ($this->getUser() != null) {
+            $em = $this->getDoctrine()->getManager();
+            $like = $em->getRepository(UserLike::class)
+                ->findOneBy([
+                    'user' => $this->getUser(),
+                    'article' => $article->getId()
+                ]);
+            if (!$like) {
+                $like = new UserLike();
+                $like->setUser($this->getUser())
+                    ->setArticle($article);
+                $em->persist($like);
+            } else {
+                $em->remove($like);
+            }
+            $em->flush();
+
+            return $this->redirect($request
                 ->headers
                 ->get('referer'));
-
-            $id = $request->get('post_id');
-            if ($id !== null) {
-                $em = $this->getDoctrine()->getManager();
-
-                $article = $em->getRepository(Article::class)
-                    ->find($id);
-                if (!$article) {
-                    return $referer;
-                }
-
-                $user = $this->get('security.token_storage')->getToken()->getUser();
-
-                $like = $em->getRepository(UserLike::class)
-                    ->findOneBy(['user_id' => $user, 'article_id' => $article]);
-                if (!$like) {
-                    $like = new UserLike();
-                    $like->setUser($user)
-                        ->setArticle($article);
-                    $em->persist($like);
-                    $em->flush();
-
-                    return $referer;
-                } else {
-                    $em->remove($like);
-                    $em->flush();
-
-                    return $referer;
-                }
-            } else {
-                return $referer;
-            }
         } else {
-            return $this->redirectToRoute('app_login');
+            return $this->redirectToRoute('user_registration');
         }
     }
 }
