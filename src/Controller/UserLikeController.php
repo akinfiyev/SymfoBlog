d3aeb50f3eabd63ddc1b1dac0c7b693f4a654b29
sliceice <mr.sliceice@gmail.com>
2019-01-17 17:41:28 +0200

    Ajax like feature

--- src/Controller/UserLikeController.php
+++ src/Controller/UserLikeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Article;
 use App\Entity\UserLike;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -27,16 +28,31 @@ class UserLikeController extends AbstractController
                 $like->setUser($this->getUser())
                     ->setArticle($article);
                 $em->persist($like);
+                $em->flush();
+
+                return new JsonResponse(count($article->getArticleLikes()));
             } else {
                 $em->remove($like);
-            }
-            $em->flush();
+                $em->flush();
 
-            return $this->redirect($request
-                ->headers
-                ->get('referer'));
+                return new JsonResponse(count($article->getArticleLikes()));
+            }
         } else {
             return $this->redirectToRoute('user_registration');
         }
+    }
+
+    /**
+     * @Route("/like2/{id}", name="like_article2")
+     */
+    public function likeArticleAction2(Request $request, Article $article)
+    {
+        if($request->query->get('some_var_name')){
+            //make something curious, get some unbelieveable data
+            $arrData = ['output' => 'here the result which will appear in div'];
+            return new JsonResponse($arrData);
+        }
+
+        return $this->render('like.html.twig');
     }
 }
